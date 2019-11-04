b06b7ceb81f5802fd2a840600408ae7a2cb2479d
ecalventesilva <ecalventesilva@gmail.com>
2019-11-04 14:16:27 +0100

    v7
    Creado Layout:PantallaQR. Conectado "primeraPantallaAndroid" con la "pantallaQR".

--- app/src/main/java/com/example/tocheck/PrimeraPantallaAdmin.java
+++ app/src/main/java/com/example/tocheck/PrimeraPantallaAdmin.java
@@ -2,7 +2,9 @@ package com.example.tocheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PrimeraPantallaAdmin extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PrimeraPantallaAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primera_pantalla_admin);
+    }
+
+    public void pantallaQR(View view) {
+        Intent intent = new Intent(this, PantallaQR.class);
+        this.startActivity(intent);
     }
 }
