77798354af33cfc63ed9052809d2c070a090d9b4
amrMoreno <adrianmorenoruiz@gmail.com>
2019-11-04 13:34:26 +0100

    Enlacez de admin/ registro de usuario
    Enlace de registro , y la parte del admin coneciones

--- app/src/main/java/com/example/tocheck/InicioAdmin.java
+++ app/src/main/java/com/example/tocheck/InicioAdmin.java
@@ -2,7 +2,9 @@ package com.example.tocheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioAdmin extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class InicioAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_admin);
+    }
+
+    public void PantallaInicioAdmin(View view) {
+        Intent intent= new Intent(this,PrimeraPantallaAdmin .class);
+        this.startActivity(intent);
     }
 }
