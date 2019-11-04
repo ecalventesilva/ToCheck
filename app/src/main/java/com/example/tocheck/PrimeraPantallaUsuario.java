d143a3f6f234eeb91c01a01e2b33ebcb4a33afa2
amrMoreno <adrianmorenoruiz@gmail.com>
2019-11-04 09:31:02 +0100

    Menu Usuario/admin

---
+++ app/src/main/java/com/example/tocheck/PrimeraPantallaUsuario.java
@@ -0,0 +1,14 @@
+package com.example.tocheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrimeraPantallaUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primera_pantalla_usuario);
+    }
+}
