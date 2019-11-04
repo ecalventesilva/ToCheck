8137736dad233cad4472670b8bae6dc6d9999fce
ecalventesilva <ecalventesilva@gmail.com>
2019-11-04 13:35:27 +0100

    V6
    Inicio_Usuario: Botón aceptar pasa a la siguiente pantalla (Primera_Pantalla_Usuario) aún sin coger los datos de BD.

--- app/src/main/java/com/example/tocheck/InicioUsuario.java
+++ app/src/main/java/com/example/tocheck/InicioUsuario.java
@@ -2,7 +2,9 @@ package com.example.tocheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioUsuario extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class InicioUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_usuario);
+    }
+
+    public void pasarPantallaPrimeraUsuario(View view) {
+        Intent intent= new Intent(this,PrimeraPantallaUsuario.class);
+        this.startActivity(intent);
     }
 }
