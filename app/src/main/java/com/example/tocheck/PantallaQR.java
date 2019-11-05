486c34d2b8da622ad3bf726046a75e72dee3f2e4
ecalventesilva <ecalventesilva@gmail.com>
2019-11-05 18:54:18 +0100

    Generador QR
    Implementado el método que genera un código QR cuando el 'Admin' introduce una clave.

--- app/src/main/java/com/example/tocheck/PantallaQR.java
+++ app/src/main/java/com/example/tocheck/PantallaQR.java
@@ -2,13 +2,63 @@ package com.example.tocheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class PantallaQR extends AppCompatActivity {
+
+    EditText editText;
+    Button buttonGenerarQR;
+    ImageView imagenQR;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_qr);
+
+        iniciarComponentes();
+        clickButton();
+    }
+
+    private void clickButton() {
+        buttonGenerarQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               generarQRCode();
+            }
+        });
+    }
+
+    private void generarQRCode() {
+        String texto= editText.getText().toString();
+        MultiFormatWriter multiFormatWriter =new MultiFormatWriter();
+
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(texto, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder= new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imagenQR.setImageBitmap(bitmap);
+        }catch (WriterException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void iniciarComponentes() {
+        editText=(EditText) findViewById(R.id.editText3);
+        buttonGenerarQR=(Button) findViewById(R.id.buttonGenerarQR);
+        imagenQR=(ImageView) findViewById(R.id.imagenQR);
+
+
     }
 }
