523bc2af1567e4ce53cebacc92d1b0197f726339
amrMoreno <adrianmorenoruiz@gmail.com>
2019-12-10 19:20:53 +0100

    LoginUsuario
    Introducion de inicio de usuario por base de dato Firebase

--- app/src/main/java/com/example/tocheck/InicioAdmin.java
+++ app/src/main/java/com/example/tocheck/InicioAdmin.java
@@ -1,21 +1,67 @@
 package com.example.tocheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
 
 public class InicioAdmin extends AppCompatActivity {
-
+private String email="";
+private String contraseña="";
+private EditText textoEmail;
+    private EditText textoContraseña;
+    private Button botonLogin;
+ private FirebaseAuth maAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_admin);
+
+textoEmail=findViewById(R.id.textoUsuarioAdmin);
+textoContraseña=findViewById(R.id.textoContraseñaAdmin);
+botonLogin=findViewById(R.id.botonLoginAdmin);
+
+maAuth= FirebaseAuth.getInstance();
     }
 
     public void PantallaInicioAdmin(View view) {
-        Intent intent= new Intent(this,PrimeraPantallaAdmin .class);
-        this.startActivity(intent);
+        email=textoEmail.getText().toString();
+        contraseña=textoContraseña.getText().toString();
+if(!email.isEmpty()&& !contraseña.isEmpty()){
+
+    loginAdmin();
+
+}else {
+    Toast.makeText(this, "El usuario o la contraseña esta vacio", Toast.LENGTH_SHORT).show();
+}
+
+
     }
+
+    private void loginAdmin() {
+        maAuth.signInWithEmailAndPassword(email,contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(InicioAdmin.this, PrimeraPantallaAdmin.class));
+                    finish();
+                }else{
+                    Toast.makeText(InicioAdmin.this, "Error, no se puedo iniciar sesión, comprube los datos ", Toast.LENGTH_SHORT).show();
+                }
+            }
+});
+    }
+
+
 }
